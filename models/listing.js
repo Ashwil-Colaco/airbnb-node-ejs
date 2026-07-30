8071722a2ad9830d241cbd74587404bf81ad8f1a
Ashwil-Colaco <ashwilcolaco@gmail.com>
2026-07-30 23:35:46 +0530

    Phase 1: initalized the db

--- models/listing.js
+++ models/listing.js
@@ -5,8 +5,15 @@ const listingSchema = new Schema({
     title: String,
     description: String,
     image: {
-        type: String,
-        set: (v)=> v === ""? "https://res.cloudinary.com/dcfaddjtn/image/upload/v1772547011/eprpirdhcog2dimc3gf4.png" : v
+        filename:{
+            type:String,
+            default:"listimage"
+        },
+        url:{
+             type: String,
+             set: (v)=> v === ""? "https://res.cloudinary.com/dcfaddjtn/image/upload/v1772547011/eprpirdhcog2dimc3gf4.png" : v
+        }
+       
     },
     price: Number,
     location: String,
