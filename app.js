ce76b6443c1eaed1c460e379ffdd26ac325f965e
Ashwil-Colaco <ashwilcolaco@gmail.com>
2026-07-31 00:07:31 +0530

    PHASE 1: added indexroute to display all lists in listings in EJS

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("Express")
 const app = express()
 const mongoose = require("mongoose")
 const Listing = require("./models/listing.js")
+const path = require("path")
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/test')
@@ -19,19 +24,30 @@ app.get("/",(req,res)=>{
     res.send("YO WASSUP im the root")
 })
 
-app.get("/testlisting",async (req, res)=>{
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description : "By the beach",
-        price : 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    })
+// app.get("/testlisting",async (req, res)=>{
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description : "By the beach",
+//         price : 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     })
 
-    await sampleListing.save()
-    console.log("sample was saved")
-    res.send("sucessful testing")
+//     await sampleListing.save()
+//     console.log("sample was saved")
+//     res.send("sucessful testing")
+// })
+
+
+//Index Route
+app.get("/listings",async (req,res)=>{
+    const allListings = await Listing.find({})
+     console.log(allListings)  
+     res.render("listings/index.ejs",{allListings})
+  
 })
+
+
 
 app.listen(8080,()=>{
     console.log("Server is on port 8080")
